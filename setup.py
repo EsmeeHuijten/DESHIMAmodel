4cb17a254db8f9621b1e8a5c6e35366a8a350ae0
Stefanie-B <s.a.brackenhoff@student.tudelft.nl>
2020-10-02 10:55:20 +0200

    changed author

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tiempo_deshima", 
-    version="0.1.0",
-    author="Esmee Huijten (edits: Stefanie Brackenhoff)",
+    version="0.1.1",
+    author="Esmee Huijten",
     maintainer= "Stefanie Brackenhoff <s.a.brackenhoff@student.tudelft.nl>",
     description="time-dependent end-to-end model for post-process optimization",
     long_description=long_description,
