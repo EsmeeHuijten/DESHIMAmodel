2f8140454875974ebb5367e6e13ddb428aa63bdb
Stefanie-B <s.a.brackenhoff@student.tudelft.nl>
2020-11-04 18:15:29 +0100

    #9 Removed deprecated function from __init__.py

--- tiempo_deshima/__init__.py
+++ tiempo_deshima/__init__.py
@@ -2,6 +2,5 @@ from . import interface
 
 from .interface import run_tiempo
 from .interface import calcMaxObsTime
-from .interface import change_pwv_0
 from .interface import new_filterbank
 from .interface import get_dictionary
